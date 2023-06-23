4f2c45fce14fcd28c0d1e202a685c0b7f7805e4a
George <kinyanjuigeorge827@gmail.com>
2023-06-23 10:08:29 +0300

    Update user management

--- src/components/ManageUsers.jsx
+++ src/components/ManageUsers.jsx
@@ -1,62 +1,63 @@
 import React from "react";
 import NewUser from "./dashboard/NewUser";
 import PersonIcon from "../assets/undraw.png";
+import PersonEdit from "../assets/Vector.png";
 import "../App.css";
 import Data from "./Data";
 const ManageUsers = () => {
   const data = Data();
   return (
     <div className="my-container">
-    <div className="manage-users">
-      <div className="userinfo">
-        <div className="nav-column">
-          <input type="checkbox" />
-          <p>Name</p>
-          <p>Number</p>
-          <p>Email</p>
-          <p>Balance (ksh)</p>
-          <p>Status</p>
-          <p>Manage</p>
+      <div className="manage-users">
+        <div className="userinfo">
+          <div className="nav-column">
+            <input type="checkbox" />
+            <p>Name</p>
+            <p>Number</p>
+            <p className="email--">Email</p>
+            <p>Balance (ksh)</p>
+            <p>Status</p>
+            <p>Manage</p>
+          </div>
+          {data.map((item) => {
+            return (
+              <div className="user-" key={item.id}>
+                <input type="checkbox" className="check-box" />
 
-        </div>
-        {data.map((item) => {
-          return (
-            <div className="user-" key={item.id}>
-              <div className="check-box">
-              <input type="checkbox" />
-                <img src={item.img} alt="user" />
-              </div>
-              <div className="details-container">
-                <div className="user-info">
-                  <h3>{item.name}</h3>
+                <div className="name">
+                  <p>{item.name}</p>
+                </div>
+                <div className="number">
+                  <p>{item.number}</p>
+                </div>
+                <div className="email">
+                  <p>{item.email}</p>
+                </div>
+                <div className="balance">
                   <p>{item.cash}</p>
                 </div>
-                <div className="user-number">
-                  <p>{item.number}</p>
-                  <p>{item.time}</p>
+                <div className="status">
+                  <p>{item.status}</p>
+                </div>
+                <div className="manage">
+                  <img src={PersonEdit} alt="user" />
+                  <span>...</span>
                 </div>
               </div>
-            </div>
-          );
-        }
-        )};
-
-      </div>
-      <div className="newuser">
-        <div className="usericon">
-          <img src={PersonIcon} alt="user" />
-
+            );
+          })}
         </div>
-        <div className="new-user-section">
-          < NewUser />
-
+        <div className="newuser">
+          <div className="usericon">
+            <img src={PersonIcon} alt="user" />
+          </div>
+          <div className="new-user-section">
+            <NewUser />
+          </div>
         </div>
       </div>
-
-    </div>
     </div>
   );
 };
-
 
 export default ManageUsers;
